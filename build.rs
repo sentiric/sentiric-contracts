b05f2de4ed027edf9ec5e89ff6ba1c9ef60a8bcf
Azmi Sahin <6445951+azmisahin@users.noreply.github.com>
2025-08-13 23:45:14 +0300

    fix(build): Enable proto3 optional flag for protoc

--- build.rs
+++ build.rs
@@ -4,6 +4,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_server(true)
         .build_client(true)
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        // --- YENİ EKLENEN SATIR ---
+        .protoc_arg("--experimental_allow_proto3_optional") // Bu, 'optional' anahtar kelimesine izin verir.
         .compile(
             &[
                 "proto/sentiric/user/v1/user.proto",
@@ -11,7 +13,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 "proto/sentiric/media/v1/media.proto",
                 "proto/sentiric/tts/v1/tts.proto",
             ],
-            &["proto"], // Arama yolu olarak sadece 'proto' klasörünü kullan
+            &["proto"],
         )?;
     Ok(())
 }
